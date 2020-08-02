bbf6797b582aa7997e10be7ddef60b97c7bfb07f
rutupawar <50703054+rutupawar@users.noreply.github.com>
2020-08-02 19:44:41 +0530

    Add files via upload

---
+++ oldDeSerialize.py
@@ -0,0 +1,36 @@
+import socket
+import pickle,sys
+
+HEADERSIZE = 10
+
+if len(sys.argv) != 3: 
+	print ("Correct usage: script, IP address, port number")
+	exit() 
+IP_address = str(sys.argv[1]) 
+Port = int(sys.argv[2])
+
+s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+s.connect((IP_address, Port))
+
+while True:
+    full_msg = b''
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print("new msg len:",msg[:HEADERSIZE])
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
+
+        print(f"full message length: {msglen}")
+
+        full_msg += msg
+
+        print(len(full_msg))
+
+        if len(full_msg)-HEADERSIZE == msglen:
+            print("full msg recvd")
+            print(full_msg[HEADERSIZE:])
+            print(pickle.loads(full_msg[HEADERSIZE:]))
+            new_msg = True
+            full_msg = b""
